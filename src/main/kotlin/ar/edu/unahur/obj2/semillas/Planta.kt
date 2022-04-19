922b4aaa6e6f1fe1e5bece502b99c895858dad2c
Adrian Rojas <adrian@agropro.ag>
2022-04-18 22:37:21 -0300

    Se agrega ejercicio 4

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Planta.kt
@@ -1,18 +1,24 @@
 package ar.edu.unahur.obj2.semillas
 
- open class Planta (var altura: Double, val anioSemilla: Int) {
+open class Planta(var altura: Double, val anioSemilla: Int) {
 
-    open fun espacio(): Double  {return altura}
+    open fun espacio(): Double {
+        return altura
+    }
 
-   open fun horasSol (): Double {
+    open fun horasSol(): Double {
         return 7.0;
     }
 
-     open fun esFuerte(): Boolean {
-       return horasSol()>9.0;
+    open fun esFuerte(): Boolean {
+        return horasSol() > 9.0;
     }
 
-     open fun daSemillas(): Boolean {
-       return esFuerte();
+    open fun daSemillas(): Boolean {
+        return esFuerte();
+    }
+
+    open fun parcelaIdeal(parcela: Parcela): Boolean {
+        return true;
     }
 }
--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -1,22 +1,28 @@
 package ar.edu.unahur.obj2.semillas
 
-class Menta( altura: Double,  anioSemilla: Int): Planta(altura, anioSemilla) {
+class Menta(altura: Double, anioSemilla: Int) : Planta(altura, anioSemilla) {
 
     override fun espacio(): Double {
-       return super.altura + 1;
+        return super.altura + 1;
     }
 
     override fun daSemillas(): Boolean {
         var alturaMenta = super.altura > 0.4;
-        if(alturaMenta || esFuerte()){
+        if (alturaMenta || esFuerte()) {
             return true;
         }
         return false;
     }
 
+    override fun parcelaIdeal(parcela: Parcela): Boolean {
+        if(parcela.superficie() > 6){
+            return true;
+        }
+        return false;
+    }
 }
 
-class MentaPeperina( altura: Double,  anioSemilla: Int): Planta(altura, anioSemilla) {
+class MentaPeperina(altura: Double, anioSemilla: Int) : Planta(altura, anioSemilla) {
 
     override fun espacio(): Double {
         return super.altura * 2;
@@ -24,79 +30,99 @@ class MentaPeperina( altura: Double,  anioSemilla: Int): Planta(altura, anioSemi
 
     override fun daSemillas(): Boolean {
         var alturaMenta = super.altura > 0.4;
-        if(alturaMenta || esFuerte()){
+        if (alturaMenta || esFuerte()) {
             return true;
         }
         return false;
     }
 
+    override fun parcelaIdeal(parcela: Parcela): Boolean {
+        if(parcela.superficie() > 6){
+            return true;
+        }
+        return false;
+    }
 }
 
-class Soja( altura: Double, anioSemilla: Int): Planta(altura, anioSemilla) {
+class Soja(altura: Double, anioSemilla: Int) : Planta(altura, anioSemilla) {
 
     override fun espacio(): Double {
-        return super.altura/2;
+        return super.altura / 2;
     }
 
-    override  fun daSemillas(): Boolean {
-        var segundaCondicion = (super.altura>0.75 && super.altura<0.9) && (anioSemilla>2007);
-        if(esFuerte() || segundaCondicion ){
+    override fun daSemillas(): Boolean {
+        var segundaCondicion = (super.altura > 0.75 && super.altura < 0.9) && (anioSemilla > 2007);
+        if (esFuerte() || segundaCondicion) {
             return true;
         }
         return false;
     }
 
-
     override fun horasSol(): Double {
-        if(super.altura<0.5){
+        if (super.altura < 0.5) {
             return 6.0;
-        }
-        else if(super.altura>0.5 && super.altura<1){
+        } else if (super.altura > 0.5 && super.altura < 1) {
             return 8.0;
-        } else{
+        } else {
             return 12.0;
         }
     }
+
+    override fun parcelaIdeal(parcela: Parcela): Boolean {
+        return (parcela.horasSol == this.horasSol())
+    }
 }
 
-class SojaTransgenica( altura: Double, anioSemilla: Int): Planta(altura, anioSemilla) {
-
+class SojaTransgenica(altura: Double, anioSemilla: Int) : Planta(altura, anioSemilla) {
     override fun espacio(): Double {
-        return super.altura/2;
+        return super.altura / 2;
     }
 
-    override  fun daSemillas(): Boolean {
+    override fun daSemillas(): Boolean {
         return false;
     }
 
-
     override fun horasSol(): Double {
-        if(super.altura<0.5){
+        if (super.altura < 0.5) {
             return 6.0;
-        }
-        else if(super.altura>0.5 && super.altura<1){
+        } else if (super.altura > 0.5 && super.altura < 1) {
             return 8.0;
-        } else{
+        } else {
             return 12.0;
         }
     }
+
+    override fun parcelaIdeal(parcela: Parcela): Boolean {
+        return (parcela.cantidadMaxPlantas() == 1.0)
+    }
 }
 
-class Quinoa(altura: Double, anioSemilla: Int): Planta(altura, anioSemilla){
+class Quinoa(altura: Double, anioSemilla: Int) : Planta(altura, anioSemilla) {
     override fun espacio(): Double {
         return super.espacio()
     }
 
     override fun horasSol(): Double {
-     if(espacio()<0.3){
-        return 10.0
-     }
+        if (espacio() < 0.3) {
+            return 10.0
+        }
         return espacio()
     }
 
     override fun daSemillas(): Boolean {
-        var segundaCondicion =(super.anioSemilla>2001 && super.anioSemilla<2008) || esFuerte();
-        return segundaCondicion;
+        return (super.anioSemilla > 2001 && super.anioSemilla < 2008) || esFuerte();
+    }
+
+    override fun parcelaIdeal(parcela: Parcela): Boolean {
+        var plantas = parcela.plantas.iterator();
+
+        while(plantas.hasNext()) {
+            var planta:Planta = plantas.next();
+            if(planta.altura > 1.5) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
